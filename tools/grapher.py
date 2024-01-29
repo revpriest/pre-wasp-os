70a23b8b47be27ba89454d41767696ebfa152a2c
Pre <pre@dalliance.net>
2024-01-29 22:32:48 +0000

    First stab at the sync
    We do indeed now be able to sync.
    
    I guess delete is most urgent next,
    but not massively  so. Probably can hold
    a month's data if you don't mind the
    sync-wait.
    
    Maybe actually draw some graphs? How
    is that gonna work anyway?

--- tools/grapher.py
+++ tools/grapher.py
@@ -8,13 +8,84 @@ import io
 import time
 import pexpect
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget, QToolBar, QVBoxLayout, QWidget, QListView, QFileSystemModel, QTextEdit, QSplitter, QComboBox, QPushButton
-from PyQt5.QtCore import QDir, Qt
+from PyQt5.QtCore import QDir, Qt, QThread, pyqtSignal
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 
+
+
+class SyncThread(QThread):
+    """ A thread that's keeping the sync running but not blocking the GUI """
+        #We do this by catting them using the shell library I reckon.
+        #We assume if they are the same length then they are the same
+        #file. Presumably mostly it'll be appended-data. Can't be
+        #copying every byte of every file or trying to generate diff
+        #hashes on the watch and "ls" on the watch only gives file size
+
+    progress_updated = pyqtSignal(int)  # Define a custom signal
+
+
+    def __init__(self, parent=None):
+        super(SyncThread, self).__init__(parent)
+        self.scanned=0
+
+    def run(self):
+        year = int(self.main.year_dropdown.currentText())
+        for mode in self.main.categories:
+            res = self.main.remote_execute("cd('/flash/logs/{:04d}/{}/')".format(year,mode)).strip()
+            time.sleep(1.1)
+            res = self.main.remote_execute("ls").strip()
+            self.diff_files(year,mode,res)
+            time.sleep(1.1)
+            res = self.main.remote_execute("collect()").strip()
+            time.sleep(1.1)
+
+
+    def diff_files(self,year=2024,mode="steps",bunchoflines=None):
+        """ We check if the file-lengths are the same as those we
+            have on record and if not then by golly we will have
+            to inform the proper authorities: IE the sync function """
+
+        #Get the current hard-drive side
+        path = "./logs/{:04d}/{}/".format(year,mode)
+        files_list = filter(lambda x : os.path.isfile(os.path.join(path,x)), os.listdir(path))
+        existing = {}
+        for f in files_list:
+            size = os.stat(os.path.join(path, f)).st_size
+            existing[f] = size
+
+        #Compare
+        lines = bunchoflines.split("\n")
+        for l in lines:
+            fields = l.strip().split()
+            if(len(fields)>1):
+                if((not fields[1] in existing.keys())or(existing[fields[1]]!=int(fields[0]))):
+                    print("Different, syncing: "+fields[1])
+                    self.sync_file(year,mode,fields[1])
+                else:
+                    #print("Already got a good looking "+fields[1])
+                    pass
+                self.scanned+=1
+                self.progress_updated.emit(self.scanned)
+        
+            
+    def sync_file(self,year,mode,fname):
+        cmd = "cat(\""+("/flash/logs/{:04d}/{}/{}".format(year,mode,fname))+"\")"
+        bunchoflines = self.main.remote_execute(cmd)
+        bunchoflines = bunchoflines.replace('\r', '').strip()+"\n"
+        time.sleep(1)
+        self.main.remote_execute("collect()")
+        with open("./logs/{:04d}/{}/{}".format(year,mode,fname), 'w') as f:
+            f.write(bunchoflines)
+        time.sleep(0.5)
+
+
+
+
 class Monographer(QMainWindow):
-""" A program for getting and graphing the monolith and mood watch log data """
+    """ A program for getting and graphing the monolith and mood watch log data """
+    categories = ["mood","heart","steps"]
 
     def __init__(self):
         """ Build the UI """
@@ -31,13 +102,20 @@ class Monographer(QMainWindow):
         self.year_dropdown.addItems([str(year) for year in range(2024, 1980, -1)])
         toolbar.addWidget(self.year_dropdown)
 
-        # Add a button to the toolbar
+
+        # Add a connect button to the toolbar
         self.connect_button = QPushButton("Connect")
         toolbar.addWidget(self.connect_button)
 
-        # Add a button to the toolbar
-        sync_button = QPushButton("Sync")
-        toolbar.addWidget(sync_button)
+        # Add a sync button to the toolbar
+        self.sync_button = QPushButton("Sync")
+        self.sync_button.clicked.connect(self.sync_files)
+        toolbar.addWidget(self.sync_button)
+
+        # Add a test button to the toolbar
+        self.test_button = QPushButton("Test")
+        self.test_button.clicked.connect(self.sync_files)
+        toolbar.addWidget(self.test_button)
 
         # Add a status bar
         self.status_bar = self.statusBar()
@@ -48,9 +126,8 @@ class Monographer(QMainWindow):
         self.setCentralWidget(self.tabs)
 
         # Add tabs
-        self.tabs.addTab(self.create_tab('steps'), 'Steps')
-        self.tabs.addTab(self.create_tab('heart'), 'Heart')
-        self.tabs.addTab(self.create_tab('mood'), 'Mood')
+        for i in self.categories:
+            self.tabs.addTab(self.create_tab(i), i)
         self.update_ui()
 
     def create_tab(self, name):
@@ -115,20 +192,16 @@ class Monographer(QMainWindow):
 
     def update_ui(self):
         """ Set the UI up depending on current state """
-        if(hasattr(self,"console")):
-          print("Updating ui with console ")
-        else:
-          print("Updating ui with no console")
 
-        if(hasattr(self,"macaddr")):
-          print("Updating ui with mac "+str(self.macaddr))
-        else:
-          print("Updating ui with no mac")
-    
+        #The connect/disconnect button
         if((hasattr(self,"console")) and (hasattr(self,"macaddr")) and 
            (self.console!=None) and (self.macaddr!=None) and (self.macaddr!="")):
             self.connect_button.setText("Disconnect")
             self.connect_button.clicked.connect(self.disconnect_device)
+            if(hasattr(self,"sync_thread")):
+                self.sync_button.setEnabled(False) 
+            else:
+                self.sync_button.setEnabled(True) 
             try:
               self.connect_button.clicked.disconnect(self.connect_device)
             except:
@@ -136,10 +209,12 @@ class Monographer(QMainWindow):
         else:
             self.connect_button.setText("Connect")
             self.connect_button.clicked.connect(self.connect_device)
+            self.sync_button.setEnabled(False) 
             try:
               self.connect_button.clicked.disconnect(self.disconnect_device)
             except:
               pass
+
             
 
     def disconnect_device(self):
@@ -172,12 +247,18 @@ class Monographer(QMainWindow):
 
         self.console.expect('Exit console using Ctrl-X')
         time.sleep(0.5)
-        test = self.remote_execute("print('hi')").strip()
-        if(test=="hi"):
+        res = self.remote_execute("from shell import ls, cd, cat").strip()
+        time.sleep(0.5)
+        res = self.remote_execute("from gc import collect").strip()
+        time.sleep(0.5)
+        res = self.remote_execute("collect()").strip()
+        if(res==""):
             self.status_bar.showMessage("connected and tested:"+self.macaddr)
         else:
-            self.status_bar.showMessage("connected and tested badly:"+self.macaddr+" -> "+str(test))
+            self.status_bar.showMessage("connected and tested badly:"+self.macaddr+" -> "+str(res))
         self.update_ui()
+
+
 
 
     def remote_execute(self,cmd):
@@ -190,6 +271,28 @@ class Monographer(QMainWindow):
         result = self.console.before.replace("\n\n\n","\n").strip("\n")
         return result
 
+
+
+    def sync_files(self):
+        """ Start the thread to pull files from the watch into our own log directory """#
+        self.sync_thread = SyncThread(self)
+        self.sync_thread.main = self
+        self.sync_thread.finished.connect(self.sync_files_finished)
+        self.sync_thread.progress_updated.connect(self.update_progress)
+        self.sync_button.setEnabled(False) 
+        self.sync_button.setText("Syncing")
+        self.sync_thread.start()
+
+    def sync_files_finished(self):
+        del(self.sync_thread)
+        self.sync_button.setText("Sync")
+        self.sync_button.setEnabled(True) 
+        self.update_ui()
+
+    def update_progress(self,x):
+        self.sync_button.setText("Synced:"+str(x))
+        self.sync_button.setEnabled(False) 
+        self.update_ui()
 
 
 ### MAIN start the app ###
